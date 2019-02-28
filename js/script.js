a7dc602d521b5f9d1a531612f64566734a459a87
timichal <michal@zlatkovsky.cz>
2019-02-28 19:11:35 +0100

    MVP

--- js/script.js
+++ js/script.js
@@ -1,45 +1,84 @@
 import "./targetblank"; // pro otvírání odkazů v novém okně
-import { h, render } from "preact";
+import { h, render, Component } from "preact";
 /** @jsx h */
 import data from "./data";
 import { getAge } from "./datehelper";
 
-/*
-const Calendar = () => (
-  <div>
-    {Object.entries(data).map(week => (
-      <div>
-        <div className="separator" id={`sep${week[0]}`}>• • •</div>
-        <WeekTitle week={week} />
-        <WeekText
-          text={week[1].text}
-        />
-        <WeekDiv week={week} />
-      </div>
-    ))}
-  </div>
-);
-*/
-const Entry = ({ candidate }) => {
+const DisplayBox = ({ candidate, handleClick }) => {
   const imgName = `${candidate.name.toLowerCase().replace(" ", "_")}.jpg`;
 
   return (
-    <div className="entry">
-      <img className="entry-img" alt={candidate.name} src={`http://data.irozhlas.cz/us-prezident-kandidati/fotky/${imgName}`} />
-      <div className="entry-name">{candidate.name}</div>
-      <div className="entry-age">{`${getAge(candidate.dob)} let`}</div>
-      <div className="entry-desc">{candidate.desc}</div>
+    <div className="displaybox">
+      <div role="button" tabIndex="0" className="displaybox-close" onClick={() => handleClick("close")}>✕</div>
+      <img className="displaybox-img" alt={candidate.name} src={`http://data.irozhlas.cz/us-prezident-kandidati/fotky/${imgName}`} />
+      <div className="displaybox-header">
+        <div className="displaybox-name">{candidate.name}</div>
+        <div className="displaybox-age">{`${getAge(candidate.dob)} let`}</div>
+      </div>
+      <div className="displaybox-desc" dangerouslySetInnerHTML={{ __html: candidate.desc }} />
     </div>
   );
 };
 
-const Tablo = () => (
-  <div id="tablo">
-    {data.map(candidate => (
-      <Entry candidate={candidate} />
-    ))}
-  </div>
-);
+
+const Entry = ({ candidate, handleClick }) => {
+  const imgName = `${candidate.name.toLowerCase().replace(" ", "_")}.jpg`;
+
+  return (
+    <div className="entry" role="button" tabIndex="0" onClick={() => handleClick(candidate)}>
+      <img className="entry-img" alt={candidate.name} src={`http://data.irozhlas.cz/us-prezident-kandidati/fotky/${imgName}`} />
+      <div className="entry-info">
+        <div className="entry-name">{candidate.name}</div>
+        <div className="entry-age">{`${getAge(candidate.dob)} let`}</div>
+        <div className="entry-desc">{candidate.desc.split("<p>")[1]}</div>
+      </div>
+    </div>
+  );
+};
+
+class Tablo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibleBox: false,
+      selectedCand: "Cory Booker",
+    };
+    this.handleEntryClick = this.handleEntryClick.bind(this);
+    this.handleBoxClick = this.handleBoxClick.bind(this);
+  }
+
+  handleEntryClick(candidate) {
+    this.setState({
+      visibleBox: true,
+      selectedCand: candidate,
+    });
+  }
+
+  handleBoxClick(action) {
+    if (action === "close") {
+      this.setState({
+        visibleBox: false,
+      });
+    }
+  }
+
+  render() {
+    const { visibleBox, selectedCand } = this.state;
+    const displayBox = visibleBox
+      ? <DisplayBox candidate={selectedCand} handleClick={this.handleBoxClick} />
+      : null;
+
+    return (
+      <div id="tablo">
+        {displayBox}
+        {data.map(candidate => (
+          <Entry candidate={candidate} handleClick={this.handleEntryClick} />
+        ))}
+      </div>
+    );
+  }
+}
+
 // ========================================
 
 render(<Tablo />, document.getElementById("kandidati"));
